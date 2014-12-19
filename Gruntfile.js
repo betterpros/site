9743aaeda2a48f8c2eb173b29afd9b31134aeb96
Lucas Bickel <hairmare@purplehaze.ch>
2014-12-19 16:10:09 +1000

    fix wiredep config

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
     wiredep: {
       target: {
         src: [
-          grunt_output_dir + '**'
+          grunt_output_dir + '**.js',
+          grunt_output_dir + '**.html',
         ]
       }
     },
